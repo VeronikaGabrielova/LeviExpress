2e7dc758892cccc2454289b32ae419341d7ca3a1
VeronikaGabrielova <gabriel.veru@gmail.com>
2023-04-07 13:39:09 +0200

    Router+vyhledávání v selectu (stavy,api) bez spoje

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,12 +1,36 @@
-import React from 'react';
-import { Footer } from '../Footer';
-import { Header } from '../Header';
-import { Home } from '../Home';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Footer } from "../Footer";
+import { Header } from "../Header";
+import { Home } from "../Home";
+import Reservation from "../Reservation";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 
 export const App = () => (
   <>
     <Header />
-    <Home />
+    <Outlet />
     <Footer />
   </>
+);
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "/reservation",
+        element: <Reservation />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.querySelector("#app")).render(
+  <RouterProvider router={router} />
 );
---
+++ src/components/Reservation/index.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+const Reservation = () => {
+return(
+    <>
+    <h2>Detail jízdenky</h2></>
+)
+}
+
+export default Reservation
