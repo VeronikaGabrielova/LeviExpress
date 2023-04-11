9c54e5d11ffd133014393837c070fdb2cf91350c
VeronikaGabrielova <gabriel.veru@gmail.com>
2023-04-11 12:17:01 +0200

    Potvrzení rezervace

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,21 +1,44 @@
 import React, { useState } from "react";
 import { JourneyPicker } from "../JourneyPicker";
 import JourneyDetail from "../JourneyDetail";
+import { useNavigate } from "react-router-dom";
 
 export const Home = () => {
+  const navigate = useNavigate();
   const [journey, setJourney] = useState(null);
 
   const handleJourneyChange = (journey) => {
     console.log(journey);
     setJourney(journey);
   };
-  console.log(journey);
+  const handleBuy = () => {
+    console.log("funguju");
+    fetch(`https://apps.kodim.cz/daweb/leviexpress/api/reservation`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        action: "create",
+        seat: journey.autoSeat,
+        journeyId: journey.journeyId,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => navigate(`/reservation/${data.results.reservationId}`));
+  };
+
   return (
     <>
       <main>
         <JourneyPicker onJourneyChange={handleJourneyChange} />
 
         {journey !== null && <JourneyDetail journey={journey} />}
+        <div className="controls container">
+          <button onClick={handleBuy} className="btn btn--big" type="button">
+            Rezervovat
+          </button>
+        </div>
       </main>
     </>
   );
--- src/components/JourneyDetail/index.jsx
+++ src/components/JourneyDetail/index.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import "./style.css";
 import BusStop from "../BusStop";
+import SelectedSeat from "../SelectedSeat";
 
 const JourneyDetail = ({ journey }) => {
   return (
-    <div className="journey-detail container">
-      <h2>Podrobnosti cesty</h2>
-      <div className="stops">
-        <BusStop journey={journey} />
+    <>
+      <div className="journey-detail container">
+        <h2>Podrobnosti cesty</h2>
+        <div className="stops">
+          <BusStop journey={journey} />
+        </div>
       </div>
-    </div>
+      {journey && <SelectedSeat number={journey.autoSeat} />}
+    </>
   );
 };
 
